0d1c25a31da4017a4cae50f9eb617855c19062d7
AgustinPinto <aguspinto08@gmail.com>
2025-04-28 16:57:39 -0300

    ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -4,6 +4,6 @@ def ejercicio():
     tps = int(input("Calificacion por trabajos practicos (20%): "))
     examen_in = int(input("Calificacion por el examen integrador (50%): "))
 
-    print("Nota A: ",examenes," Nota B: ",tps," Nota C: ",examen_in," Nota final: ",((examenes*30)/100)+((tps*20)/100)+((examen_in*50)/100))
+    print("Nota A: ",examenes," Nota B: ",tps," Nota C: ",examen_in," Nota final: ",(examenes*0.3)+(tps*0.2)+(examen_in*0.5))
     
 ejercicio()
