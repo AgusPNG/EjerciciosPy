e7b2475b33d90f5c8bb4233b20454e029b5097d6
AgustinPinto <aguspinto08@gmail.com>
2025-04-29 08:29:54 -0300

    ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,12 +1,9 @@
 def ejercicio():
-    
-    n1 = int(input("ingrese un numero (1/6): "))
-    n2 = int(input("ingrese un numero (2/6): "))
-    n3 = int(input("ingrese un numero (3/6): "))
-    n4 = int(input("ingrese un numero (4/6): "))
-    n5 = int(input("ingrese un numero (5/6): "))
-    n6 = int(input("ingrese un numero (6/6): "))
+    l=0
 
-    print("El promedio de los 6 numeros es ", (n1+n2+n3+n4+n5+n6)/6)
+    for n in range(0,6):
+        l = l + int(input("pone un numero: "))
+
+    print("El promedio de los 6 numeros es ", int(l/6))
 
 ejercicio()
