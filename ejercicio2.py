6c2235b869c2b61c1975a473e00200b0202c409c
AgustinPinto <aguspinto08@gmail.com>
2025-04-30 10:08:29 -0300

    ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,10 +1,9 @@
 def ejercicio():
+    l = 0
 
-    n1 = int(input("Ingrese un numero (1/4): "))
-    n2 = int(input("Ingrese un numero (2/4): "))
-    n3 = int(input("Ingrese un numero (3/4): "))
-    n4 = int(input("Ingrese un numero (4/4): "))
+    for n in range(1,5):
+        l = l + int(input("Ingrese un numero"))
 
-    print("la suma entre ",n1,",",n2,",",n3,",",n4," es ",n1+n2+n3+n4)
+    print("El resultado es ",l)
     
 ejercicio()
