9a7afc7efb811f15ba16f88a2b180e46d967be4e
AgustinPinto <aguspinto08@gmail.com>
2025-04-30 13:19:52 -0300

    ejercicio11.py

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,12 +1,10 @@
 def ejercicio():
     
     autos = int(input("Autos vendidos: "))
-    valores = [0]*autos
     salario = 0
 
     for n in range(0,autos):
-        valores[n] = int(input("Valor del auto: "))
-        salario += ((valores[n]*0.05)+200)
+        salario += (int(input("Valor del auto:"))*0.05+200)
 
     print("Salario: ",int(5500+salario))
 
